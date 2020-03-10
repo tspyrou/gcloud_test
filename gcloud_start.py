7c421a9333f2e957b020099d7f4bf91cc38de0fb
= <=>
2020-03-10 12:48:20 -0700

    cleaned up

--- gcloud_start.py
+++ gcloud_start.py
@@ -3,7 +3,6 @@ import sys
 import array as arr
 
 def get_external_ip_from_list_return_value(retval):
-    print "ext=", retval
     inst_data = retval[1].split()
     external_ip = inst_data[4]
     return external_ip
@@ -15,23 +14,14 @@ def run_command_print_stdout(command):
 
 def run_command_remotely_ssh(user, host, keyfile, command):
     user_host=user+"@"+host
-    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
+    ssh = subprocess.Popen(["ssh", "-oStrictHostKeyChecking=no", "-i", keyfile, user_host, command],
                        shell=False,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
     result = ssh.stdout.readlines()
-    print result
-    return result
-
-def run_command_remotely_ssh(user, host, keyfile, command):
-    print user, host, keyfile, command
-    user_host=user+"@"+host
-    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-    result = ssh.stdout.readlines()
-    print result
+    if result == []:
+        error = ssh.stderr.readlines()
+        return error
     return result
 
 inst = "instance-1"
@@ -40,7 +30,7 @@ print run_command_print_stdout(["gcloud", "compute", "instances", "list"])
 print run_command_print_stdout(["gcloud", "compute", "instances", "start", "--zone", zone, inst])
 retval_started = run_command_print_stdout(["gcloud", "compute", "instances", "list"])
 ip_add = get_external_ip_from_list_return_value(retval_started)
-print ip_add
 result = run_command_remotely_ssh("tspyrou", ip_add, "~/.ssh/gcloud", "uname -a")
+print "remote result=", result
 run_command_print_stdout(["gcloud", "compute", "instances", "stop", "--zone", zone, inst])
 print run_command_print_stdout(["gcloud", "compute", "instances", "list"])
