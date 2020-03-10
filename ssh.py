eb87ea8ab05bbd6168668b8f6ed6ff18ad1153ba
= <=>
2020-03-10 12:55:49 -0700

    clean up

--- ssh.py
+++ ssh.py
@@ -3,20 +3,16 @@ import sys
 
 
 def run_command_remotely_ssh(user, host, keyfile, command):
-    print user, host, keyfile, command
     user_host=user+"@"+host
-    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
+    ssh = subprocess.Popen(["ssh", "-oStrictHostKeyChecking=no", "-i", keyfile, user_host, command],
                        shell=False,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
     result = ssh.stdout.readlines()
-    print result
+    if result == []:
+        error = ssh.stderr.readlines()
+        return error
     return result
 
-result = run_command_remotely_ssh("tspyrou", "34.94.233.163", "~/.ssh/gcloud", "uname -a")
-
-#if result == []:
-#    error = ssh.stderr.readlines()
-#    print >>sys.stderr, "ERROR: %s" % error
-#else:
-#    print result
+result = run_command_remotely_ssh("tspyrou", "34.94.27.137", "~/.ssh/gcloud", "uname -a")
+print result
