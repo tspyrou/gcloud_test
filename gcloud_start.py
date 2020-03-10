2940c6bdf5b52d5b6cda21f61b8aa53b42344934
= <=>
2020-03-10 12:24:01 -0700

    first rev works

--- gcloud_start.py
+++ gcloud_start.py
@@ -2,11 +2,10 @@ import subprocess
 import sys
 import array as arr
 
-def get_external_ip_from_start_return_value(retval):
+def get_external_ip_from_list_return_value(retval):
     print "ext=", retval
     inst_data = retval[1].split()
     external_ip = inst_data[4]
-    print external_ip
     return external_ip
 
 def run_command_print_stdout(command):
@@ -14,12 +13,34 @@ def run_command_print_stdout(command):
     result = p1.stdout.readlines()
     return result
 
+def run_command_remotely_ssh(user, host, keyfile, command):
+    user_host=user+"@"+host
+    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+    result = ssh.stdout.readlines()
+    print result
+    return result
+
+def run_command_remotely_ssh(user, host, keyfile, command):
+    print user, host, keyfile, command
+    user_host=user+"@"+host
+    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+    result = ssh.stdout.readlines()
+    print result
+    return result
+
 inst = "instance-1"
 zone = "us-west2-a"
 print run_command_print_stdout(["gcloud", "compute", "instances", "list"])
 print run_command_print_stdout(["gcloud", "compute", "instances", "start", "--zone", zone, inst])
 retval_started = run_command_print_stdout(["gcloud", "compute", "instances", "list"])
-ip_add = get_external_ip_from_start_return_value(retval_started)
+ip_add = get_external_ip_from_list_return_value(retval_started)
 print ip_add
+result = run_command_remotely_ssh("tspyrou", ip_add, "~/.ssh/gcloud", "uname -a")
 run_command_print_stdout(["gcloud", "compute", "instances", "stop", "--zone", zone, inst])
 print run_command_print_stdout(["gcloud", "compute", "instances", "list"])
--- ssh.py
+++ ssh.py
@@ -1,17 +1,22 @@
 import subprocess
 import sys
 
-HOST="www.example.org"
-# Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMAND="uname -a"
 
-ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+def run_command_remotely_ssh(user, host, keyfile, command):
+    print user, host, keyfile, command
+    user_host=user+"@"+host
+    ssh = subprocess.Popen(["ssh", "-i", keyfile, user_host, command],
                        shell=False,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
-result = ssh.stdout.readlines()
-if result == []:
-    error = ssh.stderr.readlines()
-    print >>sys.stderr, "ERROR: %s" % error
-else:
+    result = ssh.stdout.readlines()
     print result
+    return result
+
+result = run_command_remotely_ssh("tspyrou", "34.94.233.163", "~/.ssh/gcloud", "uname -a")
+
+#if result == []:
+#    error = ssh.stderr.readlines()
+#    print >>sys.stderr, "ERROR: %s" % error
+#else:
+#    print result
