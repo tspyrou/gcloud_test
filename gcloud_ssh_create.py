bc66633c3a64fa18a29f278b8300e4f0532df594
tspyrou <46945466+tspyrou@users.noreply.github.com>
2020-03-12 14:21:25 -0700

    Update gcloud_ssh_create.py

--- gcloud_ssh_create.py
+++ gcloud_ssh_create.py
@@ -10,7 +10,7 @@
     #Make a disk snapshot from the boot disk of the instance to save.
 
 
-    # make a snapshot from a manually configured instance to save
+    gcloud beta compute --project=foss-fpga-tools-ext-openroad instances create instance-1 --zone=us-west2-a --machine-type=c2-standard-8 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=281156998478-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=openroad-ic-tom-1,device-name=openroad-ic-tom-1,mode=rw,boot=yes --reservation-affinity=any# make a snapshot from a manually configured instance to save
     #gcloud compute disks snapshot instance-1 --project=foss-fpga-tools-ext-openroad --snapshot-names=snapshot-4 --zone=us-west2-a --storage-location=us
     
     # make a disk from a snapshot
