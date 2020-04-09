d60f44e594b661705d9bb41c06837eac3978443b
= <=>
2020-04-09 14:29:01 -0700

    progress

--- gcloud_jenkins_slave_agent.py
+++ gcloud_jenkins_slave_agent.py
@@ -2,25 +2,32 @@ import subprocess
 import sys
 import array as arr
 
-def get_external_ip_from_list_return_value(instance_list):
-    inst_data = instance_list[1].split()
-    external_ip = inst_data[4]
-    return external_ip
+def run_command_locally(command):
+    p1 = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = p1.stdout.readlines()
+    return result
 
-def get_instance_names(instance_list_gcloud_retval):
+def get_instance_names():
+    instance_list_gcloud_retval = run_command_locally(["gcloud", "compute", "instances", "list"])
     instance_list_gcloud_retval = instance_list_gcloud_retval[1: len(instance_list_gcloud_retval)]
     instance_list = []
     for inst_retval in instance_list_gcloud_retval:
         inst = inst_retval.split()[0]
         instance_list.append(inst.decode('utf-8'))
+    instance_list.sort()
     return instance_list
 
-def run_command_print_stdout(command):
-    p1 = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = p1.stdout.readlines()
-    return result
+def get_jenkins_agent_instance_names():
+    prefix = "openroad-public-jenkins-agent"
+    instance_names = get_instance_names()
+    agents = []
+    for inst in instance_names:
+        if inst[0:len(prefix)] == prefix:
+            agents.append(inst)
+    agents.sort()
+    return agents
 
-def run_command_remotely_ssh(user, host, keyfile, command):
+def run_command_remotely(user, host, keyfile, command):
     user_host=user+"@"+host
     ssh = subprocess.Popen(["ssh", "-oStrictHostKeyChecking=no", "-i", keyfile, user_host, command],
                        shell=False,
@@ -32,10 +39,10 @@ def run_command_remotely_ssh(user, host, keyfile, command):
         return error
     return result
 
-retval_instances = run_command_print_stdout(["gcloud", "compute", "instances", "list"])
-instances=get_instance_names(retval_instances)
-print("running instances=",instances)
-
+instances=get_instance_names()
+print("running instances=",instances," size=",len(instances))
+agents = get_jenkins_agent_instance_names()
+print("agents=",agents)
 #gcloud compute --project "foss-fpga-tools-ext-openroad" disks create "instance-1" --size "128" --zone "us-west2-a" --source-snapshot "firstjenkinsagentworks" --type "pd-ssd"
 
 #gcloud beta compute --project=foss-fpga-tools-ext-openroad instances create instance-1 --zone=us-west2-a --machine-type=c2-standard-16 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=281156998478-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=instance-1,device-name=instance-1,mode=rw,boot=yes,auto-delete=yes --reservation-affinity=any
