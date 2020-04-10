d8d8420bab964bc17c3bf3f535094c62af2eee1f
= <=>
2020-04-10 13:33:36 -0700

    create inst works

--- gcloud_jenkins_slave_agent.py
+++ gcloud_jenkins_slave_agent.py
@@ -7,17 +7,21 @@ print(gu.run_command_locally(["ls", "-al"]))
 
 unique_name = gu.create_unique_instance_name()
 
-# create disk
-#gcloud compute --project "foss-fpga-tools-ext-openroad" disks create "unique_name" --size "128" --zone "us-central1-c" --source-snapshot "firstjenkinsagentworks-docker-clean-cronv2" --type "pd-ssd"
+#gcloud compute --project "foss-fpga-tools-ext-openroad" disks create "instance-1" --size "128" --zone "us-central1-c" --source-snapshot "firstjenkinsagentworks-docker-clean-cronv2" --type "pd-ssd"
+cmd = "gcloud compute --project foss-fpga-tools-ext-openroad disks create " + unique_name + " "
+cmd = cmd + "--size 128 --zone us-central1-c --source-snapshot firstjenkinsagentworks-docker-clean-cronv2 --type pd-ssd"
+print(cmd.split())
+print(gu.run_command_locally(cmd.split()))
 
-print("creating new disk instance",unique_name)
-print(gu.run_command_locally(["gcloud", "compute", "--project", "foss-fpga-tools-ext-openroad", "disks", "create", unique_name, "--size", "128", "--zone", "us-central1-c", "--source-snapshot", "firstjenkinsagentworks-docker-clean-cronv2", "--type", "pd-ssd"]))
+#gcloud beta compute --project=foss-fpga-tools-ext-openroad instances create instance-1 --zone=us-central1-c --machine-type=c2-standard-16 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=281156998478-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=instance-1,device-name=instance-1,mode=rw,boot=yes,auto-delete=yes --reservation-affinity=any
+cmd = "gcloud beta compute --project=foss-fpga-tools-ext-openroad instances create " + unique_name + " "
+cmd = cmd + "--zone=us-central1-c --machine-type=c2-standard-16 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=281156998478-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append "
+cmd = cmd+ "--disk=name=" + unique_name +",device-name=" + unique_name
+cmd = cmd + ",mode=rw,boot=yes,auto-delete=yes --reservation-affinity=any"
+print(cmd.split())
+print(gu.run_command_locally(cmd.split()))
+
 print(gu.run_command_locally(["gcloud", "compute", "disks", "list"]))
-
-#create instance
-#gcloud beta compute --project=foss-fpga-tools-ext-openroad instances create openroad-public-jenkins-agent-1 --zone=us-central1-c --machine-type=c2-standard-16 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=281156998478-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=openroad-public-jenkins-agent-1,device-name=openroad-public-jenkins-agent-1,mode=rw,boot=yes,auto-delete=yes --reservation-affinity=any
-print("creating new instance",unique_name)
-print(gu.run_command_locally(["gcloud", "beta", "compute", "--project=foss-fpga-tools-ext-openroad", "instances", "create", unique_name, "--zone=us-central1-c", "--machine-type=c2-standard-16", "--subnet=default", "--network-tier=PREMIUM", "--maintenance-policy=MIGRATE", "--service-account=281156998478-compute@developer.gserviceaccount.com", "--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append", "--disk-name="+unique_name, "device-name="+unique_name, "mode=rw,boot=yes,auto-delete=yes", "--reservation-affinity=any"]))
 print(gu.run_command_locally(["gcloud", "compute", "instances", "list"]))
 
 #send command
@@ -26,6 +30,7 @@ print("running command on",unique_name)
 print(gu.run_command_locally(["gcloud", "beta", "compute", "ssh",  unique_name, "--", "uname", "-a"]))
 print(gu.run_command_locally(["gcloud", "beta", "compute", "ssh",  unique_name, "--", "ls", "-al"]))
 print(gu.run_command_locally(["gcloud", "beta", "compute", "ssh",  unique_name, "--", "touch", "t.t"]))
+print(gu.run_command_locally(["gcloud", "beta", "compute", "ssh",  unique_name, "--", "whoami"]))
 
 #delete instance
 #gcloud compute instances delete unique_name --delete-disks=all --quiet
