6fe8d121975c3eebc68de074f6bff8d2d7bf9daa
Tom Spyrou <tspyrou@dfm.ucsd.edu>
2020-04-28 13:34:58 -0700

    update works

--- update_orflow.py
+++ update_orflow.py
@@ -3,23 +3,57 @@ import subprocess
 import array as arr
 import argparse
 import os
+import shlex
+
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+def check_exists(pgm):
+    if which(pgm) == None:
+        print(pgm, "not on path")
+
 
 def run_command_locally(command):
     print("command=",command)
-    subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 starting_dir = os.getcwd()
 print("path to script=",os.path.dirname(os.path.realpath(__file__)))
 print("starting_dir =",starting_dir)
+starting_dir = os.getcwd()
+print("path to script=",os.path.dirname(os.path.realpath(__file__)))
+print("starting_dir =",starting_dir)
+
 os.chdir("OpenROAD-flow")
 run_command_locally("git pull")
-os.chdir("OpenROAD-flow/flow/platforms/")
+os.chdir("flow/platforms/gf14")
 run_command_locally("git pull")
 os.chdir(starting_dir)
-os.chdir("OpenROAD-flow/flow/")
+os.chdir("OpenROAD-flow/flow/private")
 run_command_locally("git pull")
+os.chdir(starting_dir)
+os.chdir("OpenROAD-flow/tools/OpenROAD")
+run_command_locally("git pull")
+run_command_locally("git submodule update --init --recursive")
+os.chdir(starting_dir)
+os.chdir("OpenROAD-flow/tools/yosys")
+run_command_locally("git pull")
+run_command_locally("git submodule update --init --recursive")
+os.chdir(starting_dir)
+os.chdir("OpenROAD-flow/tools/TritonRoute")
+run_command_locally("git pull")
+run_command_locally("git submodule update --init --recursive")
+os.chdir(starting_dir)
+os.chdir("OpenROAD-flow/tools/TritonRoute14")
+run_command_locally("git pull")
+run_command_locally("git submodule update --init --recursive")
 os.chdir(starting_dir)
 os.chdir("OpenROAD-flow")
+run_command_locally("rm -rf tools/build")
 run_command_locally("./build_openroad.sh --latest --local")
 run_command_locally("source ./setup_env.sh")
 run_command_locally("which openroad")
