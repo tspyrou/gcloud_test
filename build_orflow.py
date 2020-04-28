dbe4ca329d0d4c47643fe36697034415babb9a43
Tom Spyrou <tspyrou@dfm.ucsd.edu>
2020-04-28 13:19:41 -0700

    flow build works and checks for tools on path works

--- build_orflow.py
+++ build_orflow.py
@@ -5,6 +5,18 @@ import argparse
 import os
 import shlex
 
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+def check_exists(pgm):
+    if which(pgm) == None:
+        print(pgm, "not on path")
+
+
 def run_command_locally(command):
     print("command=",command)
     subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -22,7 +34,7 @@ run_command_locally("git clone /home/tajayi/projects/OpenROAD/alpha-release-plat
 os.chdir(starting_dir)
 os.chdir("OpenROAD-flow")
 run_command_locally("./build_openroad.sh --latest --local")
-run_command_locally("which openroad")
-run_command_locally("which yosys")
-run_command_locally("which TritonRoute")
-run_command_locally("which TritonRoute14")
+check_exists("openroad")
+check_exists("yosys")
+check_exists("TritonRoute")
+check_exists("TritonRoute14")
--- t.py
+++ t.py
@@ -4,30 +4,28 @@ import array as arr
 import argparse
 import os
 import shlex
-import pprint
+
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+def check_exists(pgm):
+    if which(pgm) == None:
+        print(pgm, "not on path")
+
 
 def run_command_locally(command):
     print("command=",command)
-    subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 starting_dir = os.getcwd()
 print("path to script=",os.path.dirname(os.path.realpath(__file__)))
 print("starting_dir =",starting_dir)
 
-
-command = shlex.split("bash -c 'source ~/.bashrc'")
-print(command)
-proc = subprocess.Popen(command, stdout = subprocess.PIPE)
-#for line in proc.stdout:
-#  (key, _, value) = line.partition("=")
-#  os.environ[key] = value
-#proc.communicate()
-
-#pprint.pprint(dict(os.environ))
-
-run_command_locally("echo $PATH")
-#run_command_locally("bash -c [source ~/.bashrc]")
-command = shlex.split("bash -c 'source ~/.bashrc'")
-proc = subprocess.Popen(command, stdout = subprocess.PIPE)
-proc.communicate()
-print (shlex.split("bash -c 'source ~/.bashrc'"))
+check_exists("openroad")
+check_exists("yosys")
+check_exists("TritonRoute")
+check_exists("TritonRoute14")
